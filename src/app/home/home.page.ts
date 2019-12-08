b7cfa27a4d299c826368932eca94aa3b79d716ea
jack-diamond <jhdiamond96@gmail.com>
2019-12-08 15:50:56 -0800

    adding map to home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentInit, ViewChild, OnInit,  } from '@angular/core';
+import {} from 'googlemaps';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
+export class HomePage implements OnInit{
+  @ViewChild('map', {static: true}) mapElement: any;
+  map: google.maps.Map;
+  constructor() {
 
+  }
+  ngOnInit(): void {
+    const mapProperties = {
+      center: new google.maps.LatLng(35.2271, -80.8431),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
+  }
 }
